ef3d3a44b8d49b1a0df979c97b9ebbfbaaea97ae
Yash Srivastava <113162308+yashsrivastavaaa@users.noreply.github.com>
2022-10-01 19:44:28 +0530

    Add files via upload

---
+++ Scientific Calculator.py
@@ -0,0 +1,718 @@
+import math
+
+#Functions
+
+def sinf(num1):
+    a=math.sin(num1)
+    print("")
+    print("----------------------------------------")
+    print("")
+    print("sin ",num1," : ",a)
+    input()
+
+    print("Enter e to exit")
+    print("")
+    print("Enter 0 to Continue")
+    print("")
+    
+    choice_in_function = input("Enter Your Choice : ")
+
+    if choice_in_function == "e" or choice_in_function == "E":
+        exit
+
+    elif choice_in_function == "0":
+        Main_Code()
+
+    else:
+        print("")
+        print("Invalid Choice")
+        print("")
+        Main_Code()
+    
+def cosf(num1):
+    b=math.cos(num1)
+    print("")
+    print("----------------------------------------")
+    print("")
+    print("cos ",num1," : ",b)
+    input()
+
+    print("Enter e to exit")
+    print("")
+    print("Enter 0 to Continue")
+    print("")
+    
+    choice_in_function = input("Enter Your Choice : ")
+
+    if choice_in_function == "e" or choice_in_function == "E":
+        exit
+
+    elif choice_in_function == "0":
+        Main_Code()
+
+    else:
+        print("")
+        print("Invalid Choice")
+        print("")
+        Main_Code()
+    
+def tanf(num1):
+    c=math.tan(num1)
+    print("")
+    print("----------------------------------------")
+    print("")
+    print("tan ",num1," : ",c)
+    input()
+
+    print("Enter e to exit")
+    print("")
+    print("Enter 0 to Continue")
+    print("")
+    
+    choice_in_function = input("Enter Your Choice : ")
+
+    if choice_in_function == "e" or choice_in_function == "E":
+        exit
+
+    elif choice_in_function == "0":
+        Main_Code()
+
+    else:
+        print("")
+        print("Invalid Choice")
+        print("")
+        Main_Code()
+    
+def cosecf(num1):
+    d=math.sinh(num1)
+    print("")
+    print("----------------------------------------")
+    print("")
+    print("cosec ",num1," : ",d)
+    input()
+
+    print("Enter e to exit")
+    print("")
+    print("Enter 0 to Continue")
+    print("")
+    
+    choice_in_function = input("Enter Your Choice : ")
+
+    if choice_in_function == "e" or choice_in_function == "E":
+        exit
+
+    elif choice_in_function == "0":
+        Main_Code()
+
+    else:
+        print("")
+        print("Invalid Choice")
+        print("")
+        Main_Code()
+
+def secf(num1):
+    e=math.cosh(num1)
+    print("")
+    print("----------------------------------------")
+    print("")
+    print("sec ",num1," : ",e)
+    input()
+
+    print("Enter e to exit")
+    print("")
+    print("Enter 0 to Continue")
+    print("")
+    
+    choice_in_function = input("Enter Your Choice : ")
+
+    if choice_in_function == "e" or choice_in_function == "E":
+        exit
+
+    elif choice_in_function == "0":
+        Main_Code()
+
+    else:
+        print("")
+        print("Invalid Choice")
+        print("")
+        Main_Code()
+
+def cotf(num1):
+    f=math.tanh(num1)
+    print("")
+    print("----------------------------------------")
+    print("")
+    print("cot ",num1," : ",f)
+    input()
+
+    print("Enter e to exit")
+    print("")
+    print("Enter 0 to Continue")
+    print("")
+    
+    choice_in_function = input("Enter Your Choice : ")
+
+    if choice_in_function == "e" or choice_in_function == "E":
+        exit
+
+    elif choice_in_function == "0":
+        Main_Code()
+
+    else:
+        print("")
+        print("Invalid Choice")
+        print("")
+        Main_Code()
+
+def addf(num1,num2):
+    g=num1+num2
+    print("")
+    print("----------------------------------------")
+    print("")
+    print(num1," + ",num2," = ",g)
+    input()
+
+    print("Enter e to exit")
+    print("")
+    print("Enter 0 to Continue")
+    print("")
+    
+    choice_in_function = input("Enter Your Choice : ")
+
+    if choice_in_function == "e" or choice_in_function == "E":
+        exit
+
+    elif choice_in_function == "0":
+        Main_Code()
+
+    else:
+        print("")
+        print("Invalid Choice")
+        print("")
+        Main_Code()
+
+def differencef(num1,num2):
+    h=num1-num2
+    print("")
+    print("----------------------------------------")
+    print("")
+    print(num1," - ",num2," = ",h)
+    input()
+
+    print("Enter e to exit")
+    print("")
+    print("Enter 0 to Continue")
+    print("")
+    
+    choice_in_function = input("Enter Your Choice : ")
+
+    if choice_in_function == "e" or choice_in_function == "E":
+        exit
+
+    elif choice_in_function == "0":
+        Main_Code()
+
+    else:
+        print("")
+        print("Invalid Choice")
+        print("")
+        Main_Code()
+
+def productf(num1,num2):
+    i=num1*num2
+    print("")
+    print("----------------------------------------")
+    print("")
+    print(num1," * ",num2," = ",i)
+    input()
+
+    print("Enter e to exit")
+    print("")
+    print("Enter 0 to Continue")
+    print("")
+    
+    choice_in_function = input("Enter Your Choice : ")
+
+    if choice_in_function == "e" or choice_in_function == "E":
+        exit
+
+    elif choice_in_function == "0":
+        Main_Code()
+
+    else:
+        print("")
+        print("Invalid Choice")
+        print("")
+        Main_Code()
+
+def divisionf(num1,num2):
+    j=num1/num2
+    print("")
+    print("----------------------------------------")
+    print("")
+    print(num1," / ",num2," = ",j)
+    input()
+
+    print("Enter e to exit")
+    print("")
+    print("Enter 0 to Continue")
+    print("")
+    
+    choice_in_function = input("Enter Your Choice : ")
+
+    if choice_in_function == "e" or choice_in_function == "E":
+        exit
+
+    elif choice_in_function == "0":
+        Main_Code()
+
+    else:
+        print("")
+        print("Invalid Choice")
+        print("")
+        Main_Code()
+
+def powerf(num1,num2):
+    k=num1**num2
+    print("")
+    print("----------------------------------------")
+    print("")
+    print(num1," ** ",num2," = ",k)
+    input()
+
+    print("Enter e to exit")
+    print("")
+    print("Enter 0 to Continue")
+    print("")
+    
+    choice_in_function = input("Enter Your Choice : ")
+
+    if choice_in_function == "e" or choice_in_function == "E":
+        exit
+
+    elif choice_in_function == "0":
+        Main_Code()
+
+    else:
+        print("")
+        print("Invalid Choice")
+        print("")
+        Main_Code()
+
+def floatdivf(num1,num2):
+    l=num1//num2
+    print("")
+    print("----------------------------------------")
+    print("")
+    print(num1," // ",num2," = ",l)
+    input()
+
+    print("Enter e to exit")
+    print("")
+    print("Enter 0 to Continue")
+    print("")
+    
+    choice_in_function = input("Enter Your Choice : ")
+
+    if choice_in_function == "e" or choice_in_function == "E":
+        exit
+
+    elif choice_in_function == "0":
+        Main_Code()
+
+    else:
+        print("")
+        print("Invalid Choice")
+        print("")
+        Main_Code()
+
+def sqrtf(num1):
+    m=math.sqrt(num1)
+    print("")
+    print("----------------------------------------")
+    print("")
+    print("Square Root of ",num1," : ",m)
+    input()
+
+    print("Enter e to exit")
+    print("")
+    print("Enter 0 to Continue")
+    print("")
+    
+    choice_in_function = input("Enter Your Choice : ")
+
+    if choice_in_function == "e" or choice_in_function == "E":
+        exit
+
+    elif choice_in_function == "0":
+        Main_Code()
+
+    else:
+        print("")
+        print("Invalid Choice")
+        print("")
+        Main_Code()
+
+def logf(num1):
+    n=math.log(num1)
+    print("")
+    print("----------------------------------------")
+    print("")
+    print ("Log ",num1," : ",n)
+    input()
+
+    print("Enter e to exit")
+    print("")
+    print("Enter 0 to Continue")
+    print("")
+    
+    choice_in_function = input("Enter Your Choice : ")
+
+    if choice_in_function == "e" or choice_in_function == "E":
+        exit
+
+    elif choice_in_function == "0":
+        Main_Code()
+
+    else:
+        print("")
+        print("Invalid Choice")
+        print("")
+        Main_Code()
+
+def factorialf(num1):
+    o=math.factorial(num1)
+    print("")
+    print("----------------------------------------")
+    print("")
+    print("Factorial of ",num1," : ",o)
+    input()
+
+    print("Enter e to exit")
+    print("")
+    print("Enter 0 to Continue")
+    print("")
+    
+    choice_in_function = input("Enter Your Choice : ")
+
+    if choice_in_function == "e" or choice_in_function == "E":
+        exit
+
+    elif choice_in_function == "0":
+        Main_Code()
+
+    else:
+        print("")
+        print("Invalid Choice")
+        print("")
+        Main_Code()
+
+
+def Basic_Code():
+
+    print("Enter (1) for Addition")
+    print("Enter (2) for Substraction")
+    print("Enter (3) for Multiplication")
+    print("Enter (4) for Division")
+    print("Enter (5) for Float Division")
+    print("")
+    print("Enter (0) to Go Back")
+        
+    #Input in Basic Calculations
+    print("")
+    print("----------------------------------------")
+    print("")
+
+    basic_calculation = input("Enter Your Choice : ")
+    print("")
+
+    #Conditions in Basic Calculations
+
+    if basic_calculation == "1":
+
+        num1=float(input("Enter First Number : "))
+        num2=float(input("Enter Second Number : "))
+        print("")
+        addf(num1,num2)
+
+    elif basic_calculation == "2":
+
+        num1=float(input("Enter First Number : "))
+        num2=float(input("Enter Second Number : "))
+        print("")
+        differencef(num1,num2)
+
+    elif basic_calculation == "3":
+
+        num1=float(input("Enter First Number : "))
+        num2=float(input("Enter Second Number : "))
+        print("")
+        productf(num1,num2)
+
+    elif basic_calculation == "4":
+
+        num1=float(input("Enter First Number : "))
+        num2=float(input("Enter Second Number : "))
+        print("")
+        divisionf(num1,num2)
+
+    elif basic_calculation == "5":
+        num1=float(input("Enter First Number : "))
+        num2=float(input("Enter Second Number : "))
+        print("")
+        floatdivf(num1,num2)
+
+    elif basic_calculation == "0":
+        Main_Code()
+
+    else:
+        print("Wrong Input")
+        print("")
+        Basic_Code()
+
+
+def Advanced_Code():
+    print("Enter (1) for sin")
+    print("Enter (2) for cos")
+    print("Enter (3) for tan")
+    print("Enter (4) for cosec")
+    print("Enter (5) for sec")
+    print("Enter (6) for cot")
+    print("Enter (7) for log")
+    print("Enter (8) for square root")
+    print("Enter (9) for factorial")
+    print("Enter (10) for raise to power")
+
+    print("Enter (0) to Go Back")
+    print("")
+
+    #Input in Advanced Calculations
+
+    print("")
+    print("----------------------------------------")
+    print("")
+
+    advanced_calculation = input("Enter Your Choice : ")
+    print("")
+
+    #Conditions in Advanced Calculations
+
+    if advanced_calculation == "1":
+
+        num1=float(input("Enter number in Radian : "))
+        print("")
+        sinf(num1)
+
+    elif advanced_calculation == "2":
+
+        num1=float(input("Enter number in Radian : "))
+        print("")
+        cosf(num1)
+
+    elif advanced_calculation == "3":
+
+        num1=float(input("Enter number in Radian : "))
+        print("")
+        tanf(num1)    
+
+    elif advanced_calculation == "4":
+
+        num1=float(input("Enter number in Radian : "))
+        print("")
+        cosecf(num1)
+
+    elif advanced_calculation == "5":
+
+        num1=float(input("Enter number in Radian : "))
+        print("")
+        secf(num1)
+
+    elif advanced_calculation == "6":
+
+        num1=float(input("Enter number in Radian : "))
+        print("")
+        cotf(num1)
+
+    elif advanced_calculation == "7":
+
+        num1=float(input("Enter number : "))
+        print("")
+        logf(num1)
+
+    elif advanced_calculation == "8":
+
+        num1=float(input("Enter number : "))
+        print("")
+        sqrtf(num1)
+
+    elif advanced_calculation == "9":
+
+        num1=int(input("Enter number : "))
+        print("")
+        factorialf(num1)
+
+    elif advanced_calculation == "10":
+
+        num1=float(input("Enter number on base : "))
+        num2=float(input("Enter number on power : "))
+        print("")
+        powerf(num1,num2)
+
+    elif advanced_calculation == "0":
+        Main_Code()
+
+    else:
+        print("Wrong Input")
+        print("")
+        Advanced_Code()
+
+
+def Area_Calculation_Code():
+    print("Enter (1) for Area of Triangle")
+    print("Enter (2) for Area of Square")
+    print("Enter (3) for Area of Rectangle")
+    print("Enter (4) for Area of Circle")
+    print("Enter (5) for Area of Semicircle")
+
+    print("Enter (0) to Go Back")
+    print("")
+
+    #Input in Area Calculation
+
+    print("")
+    print("----------------------------------------")
+    print("")
+
+    area_calculation = input("Enter Your Choice : ")
+
+    #Condition in Area Calculation
+
+    if area_calculation == "1":
+
+        base=float(input("Enter Base of the Triangle : "))
+
+        height=float(input("Enter Height of the Triangle : "))
+
+        area_of_triangle = 0.5*base*height
+
+        print("")
+
+        print("Area of Triangle is ",area_of_triangle," Sq. Unit")
+
+        input()
+
+        print("Enter e to exit")
+        print("")
+        print("Enter 0 to Continue")
+        print("")
+    
+        choice_in_function = input("Enter Your Choice : ")
+
+        if choice_in_function == "e" or choice_in_function == "E":
+            exit
+
+        elif choice_in_function == "0":
+            Main_Code()
+
+        else:
+            print("")
+            print("Invalid Choice")
+            print("")
+            Main_Code()
+
+    
+    
+    elif area_calculation == "2":
+
+        side=float(input("Enter Side of the Square : "))
+
+        area_of_square = side*side
+
+        print("")
+
+        print("Area of Square is ",area_of_square," Sq. Unit")
+
+    
+    
+    elif area_calculation == "3":
+
+        length=float(input("Enter length of the Square : "))
+
+        breadth=float(input("Enter breadth of the Square : "))
+
+        area_of_rectangle = length*breadth
+
+        print("")
+
+        print("Area of Rectangle is ",area_of_rectangle," Sq. Unit")
+
+    
+    
+    elif area_calculation == "4":
+
+        radius=float(input("Enter Radius of the Circle : "))
+
+        area_of_circle = 3.14*radius*radius
+
+        print("")
+
+        print("Area of Circle is ",area_of_circle," Sq. Unit")
+
+    
+    
+    elif area_calculation == "5":
+
+        radius =float(input("Enter Radius of the Semi-Circle : "))
+
+        area_of_semicircle = 0.5*3.14*radius*radius
+
+        print("")
+
+        print("Area of Semi-Circle is ",area_of_semicircle," Sq. Unit")
+
+    
+    
+    elif area_calculation == "0":
+        Main_Code()
+
+    
+    
+    else:
+        print("Wrong Input")
+        print("")
+        Area_Calculation_Code()
+
+
+def Main_Code():
+    print("")
+    print("")
+    print("----------------------------------------")
+    print("")
+    print("Enter (1) for Basic Calculations")
+    print("Enter (2) for Advanced Calculations")
+    print("Enter (3) for Area Calculations")
+    print("")
+    print("")
+    
+    
+    calculation_type = input("Enter Your Choice : ")
+    print("")
+
+
+    if calculation_type == "1":
+        Basic_Code()
+
+
+    elif calculation_type == "2":
+        Advanced_Code()
+
+    
+    elif calculation_type == "3":
+        Area_Calculation_Code()
+
+
+    else:
+        Main_Code()
+
+Main_Code()
+
